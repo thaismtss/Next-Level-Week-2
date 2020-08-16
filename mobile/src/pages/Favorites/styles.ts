1fdb2e8373020f366fbc8d89986ccdcd251c6805
Thais Martins <thaismartins1999@gmail.com>
2020-08-15 22:07:19 -0300

    projeto nlw

---
+++ mobile/src/pages/Favorites/styles.ts
@@ -0,0 +1,15 @@
+import { StyleSheet } from "react-native";
+
+
+const styles = StyleSheet.create({
+    container: {
+        flex: 1,
+        borderRightColor: '#f0f0f7',
+    },
+   
+    teacherList: {
+        marginTop: -40,
+    }
+})
+
+export default styles;
